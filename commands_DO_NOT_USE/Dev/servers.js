bb9e53714faaf50dae89ed84e416c5260ba232ee
Baltraz <86964184+Killermaschine88@users.noreply.github.com>
2021-12-24 22:14:07 +0000

    server list

---
+++ commands_DO_NOT_USE/Dev/servers.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const config = require('../../constants/Bot/config.json');
+
+module.exports = {
+	name: 'servers',
+	description: 'Show Command Uses',
+	usage: 'cu',
+	perms: 'Dev',
+	folder: 'Dev',
+	aliases: ['se'],
+	async execute(client, message, args, mclient) {
+		if (message.author.id !== config.ownerID) return message.channel.send("Can't use this!");
+
+    let guilds = client.guilds.cache.map(g => ({ name: g.name, count: g.memberCount }))
+      guilds = guilds.sort((a, b) => b.count - a.count)
+
+    let str = ''
+    let i = 0
+
+    for(const g of guilds) {
+      str += `${g.name} - ${g.count}\n`
+      if(i <= 20) return
+      i++
+    }
+
+    const embed = new Discord.MessageEmbed()
+    .setTitle('20 Biggest Servers')
+    .setColor('GREEN')
+    .setDescription(`${str}`)
+
+    message.channel.send({embeds: [embed]})
+  }
+}
